c4f8741f39125c8715769b8723ef502dc28c9a23
unknown <yash.srivastava>
2020-07-31 15:15:08 +0530

    commoitted

---
+++ arr.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+class arr
+{
+	public static void main(String args[])
+	{
+	Scanner sc=new Scanner(System.in);
+	
+	//System.out.println("enter the size");
+	//x=sc.nextInt();
+
+	int[] arr=new int[5];
+	for(i=0;i<x;i++)
+	{
+	System.out.println("ff");
+	arr[i]=sc.nextInt();
+	}
+	for(int j:arr){
+	System.out.println(j);
+	}
+	}
+}
